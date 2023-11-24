eb7c00e2d3b8f8d1a09799903f12f547daccb1e9
Salju <61861078+Jusey1@users.noreply.github.com>
2023-11-23 20:28:38 -0500

    Add files via upload

--- src/main/java/net/freedinner/display/block/AbstractStackableBlock.java
+++ src/main/java/net/freedinner/display/block/AbstractStackableBlock.java
@@ -1,20 +1,11 @@
 package net.freedinner.display.block;
 
-import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.core.Direction;
-import net.minecraft.core.BlockPos;
 
 import net.freedinner.display.init.DisplayProperties;
 
@@ -27,28 +18,18 @@ public abstract class AbstractStackableBlock extends AbstractItemBlock {
 	}
 
 	@Override
-	public InteractionResult use(BlockState state, Level world, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
-		if (world instanceof ServerLevel lvl) {
-			ItemStack stack = player.getItemInHand(hand);
-			int i = state.getValue(STACKS);
-			if (stack.getItem() == this.getStackFor().getItem() && i < getMaxStackable()) {
-				world.setBlock(pos, this.defaultBlockState().setValue(FACING, state.getValue(FACING)).setValue(WATERLOGGED, state.getValue(WATERLOGGED)).setValue(STACKS, Integer.valueOf(i + 1)), 2);
-				lvl.playSound(null, pos, state.getSoundType(world, pos, player).getPlaceSound(), SoundSource.BLOCKS, 1.0F, (float) (0.8F + (Math.random() * 0.2)));
-				if (!player.isCreative()) {
-					stack.shrink(1);
-				}
-				player.swing(hand, true);
-				return InteractionResult.SUCCESS;
-			}
-		}
-		return InteractionResult.FAIL;
-	}
-
-	@Override
 	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
 		super.createBlockStateDefinition(builder);
 		builder.add(STACKS);
 	}
 
-	abstract int getMaxStackable();
+	public int getStacks(BlockState state) {
+		return state.getValue(STACKS);
+	}
+
+	public BlockState getState(BlockState state) {
+		return this.defaultBlockState().setValue(FACING, state.getValue(FACING)).setValue(WATERLOGGED, state.getValue(WATERLOGGED)).setValue(STACKS, Integer.valueOf(state.getValue(STACKS) + 1));
+	}
+
+	public abstract int getMaxStackable();
 }
--- src/main/java/net/freedinner/display/events/DisplayEvents.java
+++ src/main/java/net/freedinner/display/events/DisplayEvents.java
@@ -3,10 +3,11 @@ package net.freedinner.display.events;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.eventbus.api.Event.Result;
 import net.minecraftforge.event.level.LevelEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.item.context.UseOnContext;
@@ -20,24 +21,48 @@ import net.minecraft.core.BlockPos;
 import net.freedinner.display.util.BlockAssociations;
 import net.freedinner.display.init.DisplayTags;
 import net.freedinner.display.init.DisplayConfig;
+import net.freedinner.display.block.AbstractStackableBlock;
 import net.freedinner.display.block.AbstractItemBlock;
 
 import java.util.List;
 
 @Mod.EventBusSubscriber
 public class DisplayEvents {
+	private static BlockHitResult rez;
+
 	@SubscribeEvent
-	public static void onRightClickBlock(PlayerInteractEvent.RightClickBlock event) {
+	public static void onCheck(PlayerInteractEvent.RightClickBlock event) {
 		Player player = event.getEntity();
 		ItemStack stack = event.getItemStack();
 		LevelAccessor world = event.getLevel();
-		BlockPos pos = event.getPos();
 		if (!DisplayConfig.SNEAK.get() || (DisplayConfig.SNEAK.get() && player.isCrouching())) {
-			if (stack.is(DisplayTags.DISPLAYABLE)) {
-				event.setUseBlock(Result.DENY);
-				if (world instanceof ServerLevel lvl) {
+			if (stack.is(DisplayTags.DISPLAYABLE) && world instanceof ServerLevel) {
+				rez = event.getHitVec();
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public static void onPlace(PlayerInteractEvent.RightClickItem event) {
+		Player player = event.getEntity();
+		ItemStack stack = event.getItemStack();
+		LevelAccessor world = event.getLevel();
+		if (!DisplayConfig.SNEAK.get() || (DisplayConfig.SNEAK.get() && player.isCrouching())) {
+			if (stack.is(DisplayTags.DISPLAYABLE) && world instanceof ServerLevel lvl && rez != null) {
+				BlockPos pos = rez.getBlockPos();
+				BlockState state = world.getBlockState(pos);
+				if (state.getBlock() instanceof AbstractStackableBlock target) {
+					if (stack.is(target.getStackFor().getItem()) && target.getStacks(state) < target.getMaxStackable()) {
+						world.setBlock(pos, target.getState(state), 2);
+						lvl.playSound(null, pos, state.getSoundType(world, pos, player).getPlaceSound(), SoundSource.BLOCKS, 1.0F, (float) (0.8F + (Math.random() * 0.2)));
+						if (!player.isCreative()) {
+							stack.shrink(1);
+						}
+						player.swing(event.getHand(), true);
+					}
+				} else {
 					Block target = BlockAssociations.getBlockFor(stack.getItem());
-					if (target.asItem().useOn(new UseOnContext(player, event.getHand(), event.getHitVec())) != InteractionResult.FAIL) {
+					if (target.asItem().useOn(new UseOnContext(player, event.getHand(), rez)) != InteractionResult.FAIL) {
 						lvl.playSound(null, pos.above(), target.defaultBlockState().getSoundType(world, pos.above(), player).getPlaceSound(), SoundSource.BLOCKS, 1.0F, (float) (0.8F + (Math.random() * 0.2)));
 						player.swing(event.getHand(), true);
 					}
@@ -62,4 +87,4 @@ public class DisplayEvents {
 	private static AbstractItemBlock[] findBlocks(Class<?> targets) {
 		return ForgeRegistries.BLOCKS.getValues().stream().filter(targets::isInstance).toArray(AbstractItemBlock[]::new);
 	}
-}
+}
